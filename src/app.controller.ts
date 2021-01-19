fb84d453003906e0bf681a83b18bb6e8c6d6fe73
C-limlim <cofla2020@yonsei.ac.kr>
2021-01-20 00:57:24 +0900

    GET과 POST
    get과 post의 차이를 아직 모알아먹겠지만 적어도 get을 써야하는 곳은 확실히 알았다. postman을 써보았다. 장지훈은 ㄱ을 알려주고 a를 쓰라하는 사람이다.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { STATUS_CODES } from 'http';
+import { Controller, Post, Param, Query, Body, Get } from '@nestjs/common';
+import { get, STATUS_CODES } from 'http';
 import { AppService } from './app.service';
 
-@Controller('animals')
+@Controller('posts')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/:id')
-  getHello(@Param('id') id: number): any {
 
-    return this.appService.getAnimal(id)
+  @Post()
+  getPost(@Body('title') title:string, @Body('code') code:string, @Body('text') text: string) {
+    return this.appService.potato(title, code, text);
+  } 
+
+  @Get()
+  print() {
+    return this.appService.print()
   }
+
+  @Get('/:title')
+  findPost(@Param('title') title: number) {
+    return this.appService.findPost(title);
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@nestjs/common'
 
+// @Injectable()
+// export class AppService {
+// 	animals: any
+// 	constructor() {
+// 		this.animals = {
+// 			1: { name: '감자' },
+// 			2: { name: '채림' },
+// 		}
+// 	}
+
+// 	getAnimal(id: number): string {
+// 		return this.animals[id]
+// 	}
+// }
+
 @Injectable()
 export class AppService {
-	animals: any
+	private posts
 	constructor() {
-		this.animals = {
-			1: { name: '감자' },
-			2: { name: '채림' },
+		this.posts = []
+	}
+	potato(title: string, code: string, text: string) {
+		const tmp = {
+			"title" : title,
+			"code": code,
+			"text": text
 		}
+		this.posts.push(tmp);
 	}
 
-	getAnimal(id: number): string {
-		return this.animals[id]
+	print() {
+		return this.posts;
+	}
+	
+	findPost(title) {
+		const result = this.posts.find((post) => post.title === title);
+		return result;
 	}
 }
